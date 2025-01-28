d63fabcb4a46add0aacb12a2347567c56fe784f0
MaxLucky25 <maksrad93@gmail.com>
2025-01-28 18:08:18 +0300

    Initial commit

---
+++ src/Index.ts
@@ -0,0 +1,80 @@
+import express from "express"
+import bodyParser from "body-parser"
+
+export const app = express()
+const port = process.env.PORT || 3000
+
+
+const jsonBodyMiddleware = express.json()
+app.use(jsonBodyMiddleware)
+
+
+const products =[{id:1, title:"tomato"},{id:2, title:"orange"}];
+const adresses= [{id:1, title:"Nezalejnasti 12"},{id:2, title:"Selickogo 11"}];
+
+const parserMiddleWear = bodyParser({});
+app.use(parserMiddleWear)
+
+app.get("/products",(req,res)=>{
+    if(req.query.title){
+        let searchString = req.query.title.toString();
+        res.send(products.filter(c => c.title.indexOf(searchString) > -1));
+    }else {
+        res.send(products);
+    }
+})
+app.post("/products",(req,res)=>{
+    const newProduct ={
+        id: +(new Date()),
+        title: req.body.title
+    }
+    products.push(newProduct)
+
+    res.status(201).send(newProduct)
+
+})
+app.get("/products/:id",(req, res)=>{
+    let product = products.find(p=>p.id === +req.params.id)
+    if(product){
+        res.send(product);
+    }else{
+        res.send(404);
+    }
+})
+app.put("/products/:id",(req, res)=>{
+    let product = products.find(p=>p.id === +req.params.id)
+    if(product){
+        product.title = req.body.title
+        res.send(product);
+    }else{
+        res.send(404);
+    }
+})
+app.delete("/products/:id",(req, res)=>{
+    for(let i =0; i<products.length; i++){
+        if(products[i].id === +req.params.id){
+            products.splice(i,1);
+            res.send(204);
+            return;
+        }
+    }
+    res.send(404);
+})
+app.get("/addresses",(req, res)=>{
+    res.send(adresses);
+})
+app.get("/addresses/:id",(req, res)=>{
+    let address =adresses.find(p => p.id === +req.params.id)
+    if(address){
+        res.send(address);
+    }else{
+        res.send(404);
+    }
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
